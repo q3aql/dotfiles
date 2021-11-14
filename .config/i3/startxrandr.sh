946d96d0a76bb495cc23c6998a8de0797c56a9e9
q3aql <q3aql@protonmail.ch>
2021-11-14 01:41:46 +0100

    Dotfiles config update (2021-11-14)

--- .config/i3/startxrandr.sh
+++ .config/i3/startxrandr.sh
@@ -2,10 +2,13 @@
 
 scan_rel=0
 
-while [ ${scan_rel} -eq 0 ] ; do
-  # Primer monitor
-  xrandr --output DisplayPort-0 --mode 1280x1024 -r 75.02
-  # Segundo monitor
-  xrandr --output DisplayPort-1 --mode 1280x1024 -r 75.02 --rotate left --right-of DisplayPort-0
-  sleep 1
-done
+#while [ ${scan_rel} -eq 0 ] ; do
+#  # Primer monitor
+#  xrandr --output DisplayPort-0 --mode 1280x1024 -r 75.02
+#  # Segundo monitor
+#  xrandr --output DisplayPort-1 --mode 1280x1024 -r 75.02 --rotate left --right-of DisplayPort-0
+#  sleep 1
+#done
+sleep 2
+xrandr --output DisplayPort-0 --mode 1280x1024 -r 75.02
+xrandr --output DisplayPort-1 --mode 1280x1024 -r 75.02 --rotate left --right-of DisplayPort-0

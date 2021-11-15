afdecd3a094bebe45a3b7c55f66aad078753730d
q3aql <q3aql@protonmail.ch>
2021-11-15 20:32:56 +0100

    Dotfiles config update (2021-11-15)

---
+++ .config/i3blocks/kernel/kernel_version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Parameters
+longNumber=2
+kernelCommand="uname -r"
+archCommand=$(uname -m)
+
+# Kernel version
+kernelVersion=""
+count=1
+
+# Extract numbers of kernel version
+kernelVersionTemp=$(uname -r | cut -d "." -f ${count})
+    kernelVersionTempDot="${kernelVersionTemp}"
+    kernelVersion="${kernelVersion}${kernelVersionTempDot}"
+    count=$(expr ${count} + 1)
+    
+while [ ${count} -le ${longNumber} ] ; do
+    kernelVersionTemp=$(uname -r | cut -d "." -f ${count})
+    kernelVersionTempDot=".${kernelVersionTemp}"
+    kernelVersion="${kernelVersion}${kernelVersionTempDot}"
+    count=$(expr ${count} + 1)
+done
+
+kernelVersionTemp=$(uname -r | cut -d "." -f ${count} | cut -d "-" -f 1)
+kernelVersionTempDot=".${kernelVersionTemp}"
+kernelVersion=${kernelVersion}${kernelVersionTempDot}
+
+# Apply arch
+kernelVersion=${kernelVersion}-${archCommand}
+echo "Kernel: Linux ${kernelVersion} "
+

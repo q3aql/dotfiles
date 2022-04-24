d38a49787c44c2874044893d4b8e1368b799ffe2
q3aql <q3aql@protonmail.ch>
2022-04-24 18:31:48 +0200

    Dotfiles config update (2022-04-24)

--- .config/qtile/checkUpdates.sh
+++ .config/qtile/checkUpdates.sh
@@ -5,16 +5,14 @@
 # 0 * * * * root /usr/bin/pacman -Sy
 # 30 * * * * root /usr/bin/pacman -Sy
 if [ -f /usr/bin/pacman ] ; then
-  num_packages=$(pacman -Qu | wc -l)
-	echo "${num_packages}"
+  pacman -Qu
 
 # Check updates on Ubuntu/Debian/Devuan
 # Note: Create cron on /etc/cron.d/checkupdates with the following lines:
 # 0 * * * * root /usr/bin/aptitude update
 # 30 * * * * root /usr/bin/aptitude update
 elif [ -f /usr/bin/aptitude ] ; then
-  num_packages=$(aptitude search "~U" | wc -l)
-	echo "${num_packages}"
+  aptitude search "~U"
 
 # Disable for other distros
 else 

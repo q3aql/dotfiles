cf5c87ac11527b87e6e1da894d92a4c6d071efcd
q3aql <q3aql@protonmail.ch>
2022-05-26 21:41:00 +0200

    Dotfiles config update (2022-05-26)

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include <X11/XF86keysym.h>
